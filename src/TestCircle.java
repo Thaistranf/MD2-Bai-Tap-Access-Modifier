43588c1f6ef0058557bfc96d22098deb140cfbd6
Thaistranf <anhnamduong9x9@gmail.com>
2023-08-15 10:59:59 +0700

    add

---
+++ src/TestCircle.java
@@ -0,0 +1,8 @@
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        System.out.println("Radius is " + circle.getRadius());
+        System.out.println("Area is " + circle.getArea());
+    }
+}
